5da1b335adb54ccb69213790028419fd0ffa3498
ms75752000 <m87719577@gmail.com>
2023-02-17 19:19:54 +0530

    Current time code uploaded

---
+++ src/main/java/time/Time.java
@@ -0,0 +1,12 @@
+package time;  
+    
+public class Time {
+	
+	      
+	  // main method  
+	  public static void main(String[] argvs)   
+	  {      
+	        System.out.println(java.time.LocalTime.now());    
+	  }      
+	
+}
